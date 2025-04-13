4481261bb63045d5b0cd7ee4fc4f5438d676db4c
thecoder <fathas235@gmail.com>
2025-04-13 22:56:02 +0300

    using if statement to determine the grade

--- script.js
+++ script.js
@@ -12,5 +12,17 @@ function generateReports(students) {
     const average = total / student.scores.length;
 
     let grade = "";
+
+    if (average >= 90) {
+      grade = "A";
+    } else if (average >= 80) {
+      grade = "B";
+    } else if (average >= 70) {
+      grade = "C";
+    } else if (average >= 60) {
+      grade = "D";
+    } else {
+      grade = "F";
+    }
   });
 }
